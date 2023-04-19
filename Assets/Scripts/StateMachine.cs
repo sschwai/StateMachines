983623f45eff3321be56d0080fdc6b0146896dfa
sschwai <sydney.schwai@gmail.com>
2023-04-19 16:46:04 -0400

    Wired up idle, walk, climb, and swim states

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -13,9 +13,11 @@ public class StateMachine : MonoBehaviour
     }
 
     public State currentState = State.idle; //default state
+    private Vector3 lastPosition;
+
     void Start()
     {
-        
+        lastPosition = transform.position;
     }
 
     void Update()
@@ -34,14 +36,21 @@ public class StateMachine : MonoBehaviour
     {
         Debug.Log(other.name + " entered");
         if (other.name == "WaterTrigger")
-        { Swimming(); }
+        {
+            Debug.Log("Swimming");
+            //Swimming();
+        }
         else if (other.name == "MountainTrigger")
-        { Climbing(); }
+        {
+            Debug.Log("Climbing");
+            //Climbing();
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
-        Debug.Log(other.name + " exited");
+        //Debug.Log(other.name + " exited");
+        Debug.Log("Walking");
     }
 
     void Swimming()
@@ -56,10 +65,20 @@ public class StateMachine : MonoBehaviour
     void Idle()
     {
         Debug.Log("I am Idle.");
+        if (lastPosition != transform.position)
+        { //if last position is not current position = movement has occured
+            Debug.Log("Moving");
+        }
+        lastPosition = transform.position; //update position when moving
     }
 
     void Walking()
     {
         Debug.Log("I am walking!");
+        if (lastPosition == transform.position)
+        {
+            Debug.Log("Stopped");
+        }
+        lastPosition = transform.position;
     }
 }
